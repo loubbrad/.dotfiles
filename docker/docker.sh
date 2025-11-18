66e9a0a498caa41a5e919f1ee92af6fcac438e76
Louis <loua19@outlook.com>
2025-11-18 14:05:06 +0900

    update docker

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,4 +1,16 @@
-docker run -it --gpus all \
-        -v $HOME/work:/home/louis/work \
-        -v $HOME/miniconda3:/home/louis/miniconda3 \
-        dev
+#!/usr/bin/env bash
+
+docker_args=(
+    -it
+    --gpus all
+)
+
+if [ -d "$HOME/work" ]; then
+    docker_args+=(-v "$HOME/work:/root/work")
+fi
+
+if [ -d "$HOME/miniconda3" ]; then
+    docker_args+=(-v "$HOME/miniconda3:/root/miniconda3")
+fi
+
+docker run "${docker_args[@]}" dev
