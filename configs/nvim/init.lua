4a601b293e3812719dbebf36654b1dcdeb6b0317
Louis <Louisb19@live.co.uk>
2025-12-14 12:01:27 +0900

    improve smart split

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -77,13 +77,24 @@ vim.g.clipboard = {
 
 -- Open new window commands
 function _G.smart_split_action(callback)
+  local curr_win = vim.api.nvim_get_current_win()
   local curr_buf = vim.api.nvim_get_current_buf()
   local curr_pos = vim.api.nvim_win_get_cursor(0)
 
-  if vim.fn.winnr('$') == 1 then
-    vim.cmd('vsplit')
+  local target_win = nil
+
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    if win ~= curr_win and vim.bo[buf].filetype ~= 'oil' then
+      target_win = win
+      break
+    end
+  end
+
+  if target_win then
+    vim.api.nvim_set_current_win(target_win)
   else
-    vim.cmd('wincmd w') 
+    vim.cmd('vsplit')
   end
 
   vim.api.nvim_set_current_buf(curr_buf)
