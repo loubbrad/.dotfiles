3ab33634eaface0dce99b506c903f7578592ba56
Louis <loua19@outlook.com>
2025-12-17 13:45:36 +0900

    fix ts

--- configs/nvim/lua/plug/ts.lua
+++ configs/nvim/lua/plug/ts.lua
@@ -1,6 +1,13 @@
 MiniDeps.add('nvim-treesitter/nvim-treesitter')
-require('nvim-treesitter.configs').setup({
-  ensure_installed = { "python", "lua", "bash" },
-  highlight = { enable = true },
-})
+
+MiniDeps.later(function()
+  local ok, ts_configs = pcall(require, 'nvim-treesitter.configs')
+  if ok then
+    ts_configs.setup({
+      ensure_installed = { "python", "lua", "bash" },
+      highlight = { enable = true },
+    })
+    vim.cmd('TSUpdate')
+  end
+end)
 
