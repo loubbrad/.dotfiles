da5cb6cae4288ff036d9827095a568f930d1ca6e
Louis <loua19@outlook.com>
2025-12-17 14:15:41 +0900

    add workaround

--- configs/nvim/lua/plug/ts.lua
+++ configs/nvim/lua/plug/ts.lua
@@ -1,13 +1,12 @@
 MiniDeps.add('nvim-treesitter/nvim-treesitter')
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { "python", "lua", "bash" },
+  highlight = { enable = true },
+})
 
-MiniDeps.later(function()
-  local ok, ts_configs = pcall(require, 'nvim-treesitter.configs')
-  if ok then
-    ts_configs.setup({
-      ensure_installed = { "python", "lua", "bash" },
-      highlight = { enable = true },
-    })
-    vim.cmd('TSUpdate')
-  end
-end)
+-- This manual install fixes things...
+-- rm -rf ~/.local/share/nvim/site/pack/deps/start/nvim-treesitter
+-- git clone https://github.com/nvim-treesitter/nvim-treesitter ~/.local/share/nvim/site/pack/deps/start/nvim-treesitter
+-- cd ~/.local/share/nvim/site/pack/deps/start/nvim-treesitter
+-- git checkout v0.9.2
 
