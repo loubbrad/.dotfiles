6fecd542bde1f4d2268b94e8b87e2dd44f049b03
Louis <loua19@outlook.com>
2026-04-24 15:31:25 +0000

    update containers

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,11 @@
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(CDPATH= cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-docker build \
-    --build-arg USERNAME=$(whoami) \
-    --build-arg UID=$(id -u) \
-    --build-arg DOTFILES_HASH=$(git ls-remote https://github.com/loubbrad/.dotfiles HEAD | cut -f1) \
-    --build-arg GID=$(id -g) \
-    -t dev $SCRIPT_DIR
+(
+    cd "$SCRIPT_DIR" || exit 1
+    docker build \
+        --build-arg USERNAME=$(whoami) \
+        --build-arg UID=$(id -u) \
+        --build-arg DOTFILES_HASH=$(git ls-remote https://github.com/loubbrad/.dotfiles HEAD | cut -f1) \
+        --build-arg GID=$(id -g) \
+        -t dev .
+)
